315e79f02264d93b2ab0b92304c5fe1ae1b920b4
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-04-28 23:49:59 +0200

    [tvOS] xbmc/platform
    Co-authored-by: kambala-decapitator <decapitator@ukr.net>
    Co-authored-by: Sylvain Cecchetto <cecchetto.sylvain@me.com>

---
+++ xbmc/platform/darwin/tvos/tvosShared.m
@@ -0,0 +1,73 @@
+/*
+ *  Copyright (C) 2010-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface tvosShared : NSObject
++ (NSString*)getSharedID;
++ (NSURL*)getSharedURL;
++ (BOOL) IsTVOSSandboxed;
++ (BOOL)isJailbroken;
+@end
+
+@implementation tvosShared : NSObject
+
++ (NSString *)getSharedID {
+  NSString *bundleID;
+  NSBundle *bundle = [NSBundle mainBundle];
+  if ([[bundle.bundleURL pathExtension] isEqualToString:@"appex"]) { // We're in a extension
+    // Peel off two directory levels - Kodi.app/PlugIns/MY_APP_EXTENSION.appex
+    bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
+    bundleID = bundle.bundleIdentifier;
+  } else {
+    bundleID = [NSBundle mainBundle].bundleIdentifier;
+  }
+  return [@"group." stringByAppendingString:bundleID];
+}
+
++ (NSURL*)getSharedURL {
+  NSString* sharedID = [self getSharedID];
+  if ([self isJailbroken])
+    return [[NSURL fileURLWithPath:@"/var/mobile/Library/Caches"] URLByAppendingPathComponent:sharedID];
+  else
+  {
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    NSURL* sharedUrl = [fileManager containerURLForSecurityApplicationGroupIdentifier:sharedID];
+    sharedUrl = [sharedUrl URLByAppendingPathComponent:@"Library" isDirectory:YES];
+    sharedUrl = [sharedUrl URLByAppendingPathComponent:@"Caches" isDirectory:YES];
+    return sharedUrl;
+  }
+}
+
++ (BOOL) IsTVOSSandboxed
+{
+  static int ret = -1;
+  if (ret == -1)
+  {
+    NSBundle* bundle = [NSBundle mainBundle];
+    if ([[bundle.bundleURL pathExtension] isEqualToString:@"appex"]) { // We're in a extension
+      // Peel off two directory levels - Kodi.app/PlugIns/MY_APP_EXTENSION.appex
+      bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
+    }
+
+    ret = 1;// default to "sandboxed"
+
+    // we re NOT sandboxed if we are installed in /var/mobile/Applications with greeng0blin jailbreak
+    if ([[bundle executablePath] containsString:@"/var/mobile/Applications/"])
+    {
+      ret = 0;
+    }
+  }
+  return ret == 1;
+}
+
++ (BOOL)isJailbroken {
+  return ![self IsTVOSSandboxed];
+}
+
+@end
