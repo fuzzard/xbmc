5688221b8eff472592925993550960dd87bfc3d0
fuzzard <bmurphy@bcmcs.net>
2019-05-04 17:45:20 +1000

    [darwin_embedded] Update ios-common Keyboard code for TVOS and modern obj-c standards
    Applies modern coding standards to objective-C
    Introduces support for TVOS platform

--- xbmc/platform/darwin/ios-common/IOSKeyboardView.h
+++ xbmc/platform/darwin/ios-common/IOSKeyboardView.h
@@ -12,21 +12,19 @@
 
 @interface KeyboardView : UIView <UITextFieldDelegate>
 {
-  NSMutableString *text;
-  BOOL _confirmed;
-  CIOSKeyboard *_iosKeyboard;
   bool *_canceled;
   BOOL _deactivated;
   UITextField *_textField;
   UITextField *_heading;
+#if defined(TARGET_DARWIN_IOS)
   int _keyboardIsShowing; // 0: not, 1: will show, 2: showing
+#endif
   CGRect _kbRect;
 }
 
-@property(nonatomic, strong) NSMutableString* text;
-@property (getter = isConfirmed) BOOL _confirmed;
-@property (assign, setter = registerKeyboard:) CIOSKeyboard *_iosKeyboard;
-@property CGRect _frame;
+@property (nonatomic, strong) NSMutableString* text;
+@property (getter = isConfirmed) BOOL confirmed;
+@property (assign) CIOSKeyboard* iosKeyboard;
 
 - (void) setHeading:(NSString *)heading;
 - (void) setHidden:(BOOL)hidden;
@@ -36,5 +34,4 @@
 - (void) textChanged:(NSNotification*)aNotification;
 - (void) setCancelFlag:(bool *)cancelFlag;
 - (void) doDeactivate:(NSDictionary *)dict;
-- (id)initWithFrameInternal;
 @end
