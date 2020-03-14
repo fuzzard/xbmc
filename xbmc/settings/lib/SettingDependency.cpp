d8d45b7ee0a09299ba934b711f9688c88c326fd7
montellese <montellese@kodi.tv>
2020-03-14 22:33:28 +0100

    [settings/lib] make use of new logging functionality

--- xbmc/settings/lib/SettingDependency.cpp
+++ xbmc/settings/lib/SettingDependency.cpp
@@ -20,6 +20,8 @@
 #include <stdlib.h>
 #include <string>
 
+Logger CSettingDependencyCondition::s_logger = CLog::Get("CSettingDependencyCondition");
+
 CSettingDependencyCondition::CSettingDependencyCondition(CSettingsManager *settingsManager /* = nullptr */)
   : CSettingConditionItem(settingsManager)
 { }
@@ -61,13 +63,13 @@ bool CSettingDependencyCondition::Deserialize(const TiXmlNode *node)
   auto strTarget = elem->Attribute(SETTING_XML_ATTR_ON);
   if (strTarget != nullptr && !setTarget(strTarget))
   {
-    CLog::Log(LOGWARNING, "CSettingDependencyCondition: unknown target \"%s\"", strTarget);
+    s_logger->warn("unknown target \"{}\"", strTarget);
     return false;
   }
 
   if (m_target != SettingDependencyTarget::Setting && m_name.empty())
   {
-    CLog::Log(LOGWARNING, "CSettingDependencyCondition: missing name for dependency");
+    s_logger->warn("missing name for dependency");
     return false;
   }
 
@@ -75,7 +77,7 @@ bool CSettingDependencyCondition::Deserialize(const TiXmlNode *node)
   {
     if (m_setting.empty())
     {
-      CLog::Log(LOGWARNING, "CSettingDependencyCondition: missing setting for dependency");
+      s_logger->warn("missing setting for dependency");
       return false;
     }
 
@@ -86,7 +88,7 @@ bool CSettingDependencyCondition::Deserialize(const TiXmlNode *node)
   auto strOperator = elem->Attribute(SETTING_XML_ATTR_OPERATOR);
   if (strOperator != nullptr && !setOperator(strOperator))
   {
-    CLog::Log(LOGWARNING, "CSettingDependencyCondition: unknown operator \"%s\"", strOperator);
+    s_logger->warn("unknown operator \"{}\"", strOperator);
     return false;
   }
 
@@ -112,7 +114,7 @@ bool CSettingDependencyCondition::Check() const
       auto setting = m_settingsManager->GetSetting(m_setting);
       if (setting == nullptr)
       {
-        CLog::Log(LOGWARNING, "CSettingDependencyCondition: unable to check condition on unknown setting \"%s\"", m_setting.c_str());
+        s_logger->warn("unable to check condition on unknown setting \"{}\"", m_setting);
         return false;
       }
 
@@ -162,7 +164,7 @@ bool CSettingDependencyCondition::Check() const
         setting = m_settingsManager->GetSetting(m_setting);
         if (setting == nullptr)
         {
-          CLog::Log(LOGWARNING, "CSettingDependencyCondition: unable to check condition on unknown setting \"%s\"", m_setting.c_str());
+          s_logger->warn("unable to check condition on unknown setting \"{}\"", m_setting);
           return false;
         }
       }
@@ -309,6 +311,8 @@ CSettingDependencyConditionCombination* CSettingDependencyConditionCombination::
   return this;
 }
 
+Logger CSettingDependency::s_logger = CLog::Get("CSettingDependency");
+
 CSettingDependency::CSettingDependency(CSettingsManager *settingsManager /* = nullptr */)
   : CSettingCondition(settingsManager)
 {
@@ -334,7 +338,7 @@ bool CSettingDependency::Deserialize(const TiXmlNode *node)
   auto strType = elem->Attribute(SETTING_XML_ATTR_TYPE);
   if (strType == nullptr || strlen(strType) <= 0 || !setType(strType))
   {
-    CLog::Log(LOGWARNING, "CSettingDependency: missing or unknown dependency type definition");
+    s_logger->warn("missing or unknown dependency type definition");
     return false;
   }
 
