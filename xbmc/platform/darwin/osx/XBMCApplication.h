00d12c0349b41d5446614d4ce6d629e1a1be05e7
fuzzard <fuzzard@kodi.tv>
2020-06-28 13:10:38 +1000

    [osx] XBMCApplication

---
+++ xbmc/platform/darwin/osx/XBMCApplication.h
@@ -0,0 +1,12 @@
+/*
+ *  Copyright (C) 2020 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainDelegate : NSObject <NSApplicationDelegate>
+@end
