2c2d015622a996a38880eba2b880117837973a62
fuzzard <fuzzard@kodi.tv>
2021-09-13 12:31:19 +1000

    [osx] configure/cmake support osx native
    depends support for native windowing
    cmake support for native windowing

--- cmake/platform/osx/osx.cmake
+++ cmake/platform/osx/osx.cmake
@@ -12,6 +12,8 @@ if(NOT APP_WINDOW_SYSTEM OR APP_WINDOW_SYSTEM STREQUAL sdl)
 
   list(APPEND CORE_MAIN_SOURCE ${CMAKE_SOURCE_DIR}/xbmc/platform/darwin/osx/SDLMain.mm
                                ${CMAKE_SOURCE_DIR}/xbmc/platform/darwin/osx/SDLMain.h)
+elseif(APP_WINDOW_SYSTEM STREQUAL native)
+  # native windowing and input
 else()
-  message(SEND_ERROR "Currently only SDL windowing is supported. Please set APP_WINDOW_SYSTEM to \"sdl\"")
+  message(SEND_ERROR "Only SDL or native windowing options are supported.")
 endif()
