9385f94bb90bab658fbe9d9b188171c5b03c172c
fuzzard <fuzzard@kodi.tv>
2021-10-29 16:59:03 +1000

    review: review comments fix up

--- xbmc/windowing/osx/OpenGL/OSXGLView.mm
+++ xbmc/windowing/osx/OpenGL/OSXGLView.mm
@@ -20,16 +20,24 @@
 #include "system_gl.h"
 
 @implementation OSXGLView
+{
+  NSOpenGLContext* m_glcontext;
+  NSOpenGLPixelFormat* m_pixFmt;
+  NSTrackingArea* m_trackingArea;
+  BOOL pause;
+}
 
 - (id)initWithFrame:(NSRect)frameRect
 {
-  NSOpenGLPixelFormatAttribute wattrs[] = {
+  NSOpenGLPixelFormatAttribute wattrs[] =
+  {
       NSOpenGLPFANoRecovery,    NSOpenGLPFAAccelerated,
       NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
       NSOpenGLPFAColorSize,     (NSOpenGLPixelFormatAttribute)32,
       NSOpenGLPFAAlphaSize,     (NSOpenGLPixelFormatAttribute)8,
       NSOpenGLPFADepthSize,     (NSOpenGLPixelFormatAttribute)24,
-      NSOpenGLPFADoubleBuffer,  (NSOpenGLPixelFormatAttribute)0};
+      NSOpenGLPFADoubleBuffer,  (NSOpenGLPixelFormatAttribute)0
+  };
 
   self = [super initWithFrame:frameRect];
   if (self)
@@ -55,11 +63,9 @@
 
 - (void)drawRect:(NSRect)rect
 {
-  static BOOL firstRender = YES;
-  if (firstRender)
-  {
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
     [m_glcontext setView:self];
-    firstRender = NO;
 
     // clear screen on first render
     glClearColor(0, 0, 0, 0);
@@ -67,7 +73,7 @@
     glClearColor(0, 0, 0, 0);
 
     [m_glcontext update];
-  }
+  });
 }
 
 - (void)updateTrackingAreas
--- xbmc/windowing/osx/WinSystemOSX.h
+++ xbmc/windowing/osx/WinSystemOSX.h
@@ -26,8 +26,8 @@ class CWinEventsOSX;
 @class NSWindow;
 @class OSXGLView;
 #else
-class NSWindow;
-class OSXGLView;
+struct NSWindow;
+struct OSXGLView;
 #endif
 
 class CWinSystemOSX : public CWinSystemBase, public ITimerCallback
@@ -95,8 +95,8 @@ protected:
   void EnableVSync(bool enable);
   bool SwitchToVideoMode(int width, int height, double refreshrate);
   void FillInVideoModes();
-  bool FlushBuffer(void);
-  bool IsObscured(void);
+  bool FlushBuffer();
+  bool IsObscured();
 
   bool DestroyWindowInternal();
 
