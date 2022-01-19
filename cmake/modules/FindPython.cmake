26bcadcd01c55f987ad0d284f2062454c6985575
fuzzard <fuzzard@kodi.tv>
2022-01-19 20:05:26 +1000

    [cmake] python3 internal depend build

--- cmake/modules/FindPython.cmake
+++ cmake/modules/FindPython.cmake
@@ -53,17 +53,27 @@ if(PYTHON_VER)
   unset(PYTHON_VER CACHE)
 endif()
 
-find_package(Python3 ${VERSION} ${EXACT_VER} COMPONENTS Development)
+if(NOT ENABLE_INTERNAL_PYTHON)
+  find_package(Python3 ${VERSION} ${EXACT_VER} COMPONENTS Development)
+endif()
 if(CORE_SYSTEM_NAME STREQUAL linux)
   find_package(Python3 ${VERSION} ${EXACT_VER} COMPONENTS Interpreter)
 endif()
 
-if(KODI_DEPENDSBUILD)
+if(KODI_DEPENDSBUILD OR ENABLE_INTERNAL_PYTHON)
+
+  # Python uses ax_c_float_words_bigendian.m4 to find autoconf-archive
+  # Make sure we can find it as a requirement as well
+  find_file(AUTOCONF-ARCHIVE "ax_c_float_words_bigendian.m4" PATHS "${NATIVEPREFIX}/share/aclocal" NO_CMAKE_FIND_ROOT_PATH REQUIRED)
+  string(REGEX REPLACE "/ax_c_float_words_bigendian.m4" "" AUTOCONF-ARCHIVE ${AUTOCONF-ARCHIVE})
+  set(ACLOCAL_PATH_VAR "ACLOCAL_PATH=${AUTOCONF-ARCHIVE}")
+
   find_library(FFI_LIBRARY ffi REQUIRED)
   find_library(EXPAT_LIBRARY expat REQUIRED)
   find_library(INTL_LIBRARY intl REQUIRED)
   find_library(GMP_LIBRARY gmp REQUIRED)
   find_library(LZMA_LIBRARY lzma REQUIRED)
+  find_package(OpenSSL REQUIRED)
 
   if(NOT CORE_SYSTEM_NAME STREQUAL android)
     set(PYTHON_DEP_LIBRARIES pthread dl util)
@@ -73,7 +83,88 @@ if(KODI_DEPENDSBUILD)
     endif()
   endif()
 
+  include(ExternalProject)
+  include(cmake/scripts/common/ModuleHelpers.cmake)
+
+  get_archive_name(python3)
+
+  # allow user to override the download URL with a local tarball
+  # needed for offline build envs
+  if(PYTHON3_URL)
+    get_filename_component(PYTHON3_URL "${PYTHON3_URL}" ABSOLUTE)
+  else()
+    # fixup BASE_URL to replace $(VERSION) with actual version number
+    string(REPLACE "\$\(VERSION\)" ${PYTHON3_VER} PYTHON3_BASE_URL ${PYTHON3_BASE_URL})
+    set(PYTHON3_URL ${PYTHON3_BASE_URL}/${ARCHIVE})
+  endif()
+  if(VERBOSE)
+    message(STATUS "PYTHON3_URL: ${PYTHON3_URL}")
+  endif()
+
+  # Set version major/minor
+  string(REGEX MATCH "^([0-9]?)\.([0-9]+)\." Python3_VERSION ${PYTHON3_VER})
+  set(Python3_VERSION_MAJOR ${CMAKE_MATCH_1})
+  set(Python3_VERSION_MINOR ${CMAKE_MATCH_2})
+
+  set(Python3_LIBRARIES ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/libpython${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}.a)
+  set(Python3_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/Include)
+
+  if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+    set(HOSTPLATFORM "_PYTHON_HOST_PLATFORM=\"darwin\"")
+  endif()
+
+  if(CORE_SYSTEM_NAME STREQUAL linux)
+    set(EXTRA_CONFIGURE ac_cv_pthread=yes)
+  elseif(CORE_SYSTEM_NAME STREQUAL darwin_embedded)
+    set(EXTRA_CONFIGURE ac_cv_func_wait3=no ac_cv_func_wait4=no ac_cv_func_waitpid=no)
+    list(APPEND EXTRA_CONFIGURE ac_cv_func_execv=no ac_cv_func_fexecv=no ac_cv_func_getentropy=no)
+    list(APPEND EXTRA_CONFIGURE ac_cv_func_setpriority=no ac_cv_func_sendfile=no ac_cv_header_sched_h=no)
+    list(APPEND EXTRA_CONFIGURE ac_cv_func_posix_spawn=no ac_cv_func_posix_spawnp=no)
+    list(APPEND EXTRA_CONFIGURE ac_cv_func_forkpty=no ac_cv_lib_util_forkpty=no)
+    list(APPEND EXTRA_CONFIGURE ac_cv_func_getgroups=no)
+  endif()
+
+  if(EXISTS "${CMAKE_SOURCE_DIR}/tools/depends/target/python3/01-${CORE_SYSTEM_NAME}-modules.patch")
+      set(PLATFORM_PATCH patch -p1 -i "${CMAKE_SOURCE_DIR}/tools/depends/target/python3/01-${CORE_SYSTEM_NAME}-modules.patch")
+  endif()
+
+ # MESSAGE(FATAL_ERROR "PLATFORM_PATCH: ${PLATFORM_PATCH}")
+
+  externalproject_add(python3
+                      URL ${PYTHON3_URL}
+                      URL_HASH SHA256=0a8fbfb5287ebc3a13e9baf3d54e08fa06778ffeccf6311aef821bb3a6586cc8
+                      DOWNLOAD_NAME ${ARCHIVE}
+                      DOWNLOAD_DIR ${TARBALL_DIR}
+                      PREFIX ${CORE_BUILD_DIR}/python3
+                      PATCH_COMMAND ${CMAKE_COMMAND} -E copy
+                                    ${CMAKE_SOURCE_DIR}/tools/depends/target/python3/modules.setup
+                                    <SOURCE_DIR>/Modules/Setup
+                            COMMAND patch -p1 -i ${CMAKE_SOURCE_DIR}/tools/depends/target/python3/crosscompile.patch
+                            COMMAND patch -p1 -i ${CMAKE_SOURCE_DIR}/tools/depends/target/python3/android.patch
+                            COMMAND patch -p1 -i ${CMAKE_SOURCE_DIR}/tools/depends/target/python3/apple.patch
+                            COMMAND ${PLATFORM_PATCH}
+                      CONFIGURE_COMMAND ${ACLOCAL_PATH_VAR} autoreconf -vif
+                                COMMAND ${CMAKE_COMMAND} -E env CFLAGS=${CMAKE_C_FLAGS} CPPFLAGS=${CMAKE_CPP_FLAGS} LDFLAGS=${CMAKE_EXE_LINKER_FLAGS}
+                                        ./configure
+                                        --disable-shared
+                                        --without-ensurepip
+                                        --disable-framework
+                                        --without-pymalloc
+                                        --enable-ipv6
+                                        --prefix=${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/depends/target
+                                        ${EXTRA_CONFIGURE}
+                      BUILD_COMMAND $(MAKE) ${HOSTPLATFORM} prefix=${DEPENDS_PATH} PYTHON_FOR_BUILD=${NATIVEPREFIX}/bin/python3 CROSS_COMPILE_TARGET=yes libpython${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}.a
+                      INSTALL_COMMAND $(MAKE) install
+                      BUILD_BYPRODUCTS ${Python3_LIBRARIES}
+                      BUILD_IN_SOURCE 1)
+
   list(APPEND Python3_LIBRARIES ${LZMA_LIBRARY} ${FFI_LIBRARY} ${EXPAT_LIBRARY} ${INTL_LIBRARY} ${GMP_LIBRARY} ${PYTHON_DEP_LIBRARIES})
+
+  set(Python3_FOUND TRUE)
+
+  # ToDo: build pythonmodules if KODI_DEPENDSBUILD
+
+  set_target_properties(python3 PROPERTIES FOLDER "External Projects")
 endif()
 
 if(Python3_FOUND)
