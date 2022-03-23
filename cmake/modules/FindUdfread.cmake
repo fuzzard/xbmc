8afe356cee32cdbe213f2963d3328466d1dc5ca9
fuzzard <fuzzard@kodi.tv>
2022-03-23 10:45:03 +1000

    [cmake] udfread

--- cmake/modules/FindUdfread.cmake
+++ cmake/modules/FindUdfread.cmake
@@ -19,16 +19,25 @@ if(ENABLE_INTERNAL_UDFREAD)
 
   set(UDFREAD_VERSION ${${MODULE}_VER})
 
-  set(CONFIGURE_COMMAND autoreconf -vif &&
-                        ./configure
+  set(PROJECT_NAME "udfread-build")
+
+  find_program(AUTORECONF autoreconf REQUIRED)
+
+  set(CONFIGURE_COMMAND ${DEPBUILDENV} ${AUTORECONF} -vif
+                COMMAND ${DEPBUILDENV} ./configure
                         --enable-static
                         --disable-shared
                         --prefix=${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR})
+
+  set(BUILD_COMMAND make)
+  set(INSTALL_COMMAND make install)
   set(BUILD_IN_SOURCE 1)
 
-  BUILD_DEP_TARGET()
-
-  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP udfread)
+  if(NOT TARGET udfread-build)
+    BUILD_DEP_TARGET()
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP udfread-build)
+  endif()
+  unset(PROJECT_NAME)
 else()
   if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_UDFREAD udfread>=1.0.0 QUIET)
@@ -52,6 +61,16 @@ if(UDFREAD_FOUND)
   set(UDFREAD_LIBRARIES ${UDFREAD_LIBRARY})
   set(UDFREAD_INCLUDE_DIRS ${UDFREAD_INCLUDE_DIR})
   set(UDFREAD_DEFINITIONS -DHAS_UDFREAD=1)
+
+  if(NOT TARGET udfread)
+    add_library(udfread UNKNOWN IMPORTED)
+    set_target_properties(udfread PROPERTIES
+                                  IMPORTED_LOCATION "${UDFREAD_LIBRARY}"
+                                  INTERFACE_INCLUDE_DIRECTORIES "${UDFREAD_INCLUDE_DIR}"
+                                  INTERFACE_COMPILE_DEFINITIONS "${UDFREAD_DEFINITIONS}")
+  endif()
+
+  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP udfread)
 endif()
 
 mark_as_advanced(UDFREAD_INCLUDE_DIR UDFREAD_LIBRARY)
