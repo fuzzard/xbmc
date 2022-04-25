77366c22eee93d5efe2b4859d9139d59c407bc1a
fuzzard <fuzzard@kodi.tv>
2022-04-25 15:56:56 +1000

    [cmake] Build LibZip during cmake Build
    libzip is only used by android.
    It supports cmake build system, so lets just build during core cmake build in Find modules
    rename folder to libzip to match libname

---
+++ cmake/modules/FindLibZip.cmake
@@ -0,0 +1,41 @@
+#.rst:
+# FindLibZip
+# -----------
+# Finds the LibZip library
+#
+# This will define the following variables::
+#
+# LIBZIP_FOUND - system has LibZip
+# LIBZIP_INCLUDE_DIRS - the LibZip include directory
+# LIBZIP_LIBRARIES - the LibZip libraries
+#
+
+include(cmake/scripts/common/ModuleHelpers.cmake)
+
+set(MODULE_LC libzip)
+
+SETUP_BUILD_VARS()
+
+set(CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}
+               -DCMAKE_BUILD_TYPE=Release
+               -DBUILD_DOC=OFF
+               -DBUILD_EXAMPLES=OFF
+               -DBUILD_REGRESS=OFF
+               -DBUILD_SHARED_LIBS=OFF
+               -DBUILD_TOOLS=OFF)
+
+BUILD_DEP_TARGET()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibZip
+                                  REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR
+                                  VERSION_VAR LIBZIP_VER)
+
+if(LIBZIP_FOUND)
+  set(LIBZIP_LIBRARIES ${LIBZIP_LIBRARY})
+  set(LIBZIP_INCLUDE_DIRS ${LIBZIP_INCLUDE_DIR})
+
+  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libzip)
+endif()
+
+mark_as_advanced(LIBZIP_INCLUDE_DIR LIBZIP_LIBRARY)
