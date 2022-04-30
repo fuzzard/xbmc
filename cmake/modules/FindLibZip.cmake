fbe5f61b8276cde5d85a7b52503b3c3e45901141
fuzzard <fuzzard@kodi.tv>
2022-04-30 16:09:26 +1000

    [cmake] FindLibZip fix

--- cmake/modules/FindLibZip.cmake
+++ cmake/modules/FindLibZip.cmake
@@ -12,6 +12,14 @@
 
 find_package(LIBZIP CONFIG QUIET)
 
+# LibZip dependencies
+find_package(BZip2 REQUIRED)
+find_package(GnuTLS REQUIRED)
+find_package(ZLIB REQUIRED)
+
+# GnuTLS dependency
+find_package(Nettle REQUIRED)
+
 if(NOT LIBZIP_FOUND)
   include(cmake/scripts/common/ModuleHelpers.cmake)
 
@@ -24,21 +32,27 @@ if(NOT LIBZIP_FOUND)
                  -DBUILD_EXAMPLES=OFF
                  -DBUILD_REGRESS=OFF
                  -DBUILD_SHARED_LIBS=OFF
+                 -DENABLE_LZMA=OFF
                  -DBUILD_TOOLS=OFF)
 
   BUILD_DEP_TARGET()
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LibZip
+                                    REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR
+                                    VERSION_VAR LIBZIP_VER)
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibZip
-                                  REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR
-                                  VERSION_VAR LIBZIP_VER)
-
 if(LIBZIP_FOUND)
-  set(LIBZIP_LIBRARIES ${LIBZIP_LIBRARY})
-  set(LIBZIP_INCLUDE_DIRS ${LIBZIP_INCLUDE_DIR})
+  if(NOT TARGET libzip::zip)
+    add_library(libzip::zip UNKNOWN IMPORTED)
+    set_target_properties(libzip::zip PROPERTIES
+                                 IMPORTED_LOCATION "${LIBZIP_LIBRARY}"
+                                 INTERFACE_INCLUDE_DIRECTORIES "${LIBZIP_INCLUDE_DIR}")
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libzip)
 
-  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libzip)
+  endif()
+  set(lib_TARGETS libzip::zip ${lib_TARGETS} CACHE STRING "" FORCE)
 endif()
 
 mark_as_advanced(LIBZIP_INCLUDE_DIR LIBZIP_LIBRARY)
