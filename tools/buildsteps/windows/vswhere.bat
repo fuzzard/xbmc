17013228b1f4ae223d0e7af9d9fbc245945d0ac8
fuzzard <fuzzard@kodi.tv>
2023-06-15 20:47:24 +1000

    [windows] Distinguish between host/native download/tools for windows build
    Currently the precompiled tools for windows builds are x86/win32. For now stick with
    this assumption and instead of putting these tools into the target arch folder, put them
    into the win32 folder to keep arch libs/exes in correct arch folders

--- tools/buildsteps/windows/vswhere.bat
+++ tools/buildsteps/windows/vswhere.bat
@@ -27,19 +27,10 @@ SET vcvars=no
 SET sdkver=
 
 SET vsver=
-SET toolsdir=%arch%
 
-IF "%arch%" NEQ "x64" (
-  SET vcarch=%vcarch%_%arch%
-)
-
-IF "%arch%"=="x86" (
-  SET toolsdir=win32
-)
-
-IF "%vcstore%"=="store" (
-  SET toolsdir="win10-%toolsdir%"
-)
+REM Current tools are only using x86/win32
+REM ToDo: allow to set NATIVEPLATFORM to allow native tools based on actual native arch (eg x86/x86_64/arm/arm64)
+SET toolsdir=win32
 
 SET vswhere="%builddeps%\%toolsdir%\tools\vswhere\vswhere.exe"
 
