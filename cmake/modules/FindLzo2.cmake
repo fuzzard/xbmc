00df668b5af751376312ba4ab19472a748c2bc2c
fuzzard <fuzzard@kodi.tv>
2023-09-05 18:02:39 +1000

    [cmake] FindLzo2 migrate to full TARGET usage

--- cmake/modules/FindLzo2.cmake
+++ cmake/modules/FindLzo2.cmake
@@ -3,35 +3,27 @@
 # --------
 # Finds the Lzo2 library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# LZO2_FOUND - system has Lzo2
-# LZO2_INCLUDE_DIRS - the Lzo2 include directory
-# LZO2_LIBRARIES - the Lzo2 libraries
-#
-# and the following imported targets::
-#
-#   Lzo2::Lzo2   - The Lzo2 library
+#   Lzo2::Lzo2 - The Lzo2 library
 
-find_path(LZO2_INCLUDE_DIR NAMES lzo1x.h
-                           PATH_SUFFIXES lzo)
+if(NOT TARGET Lzo2::Lzo2)
+  find_path(LZO2_INCLUDE_DIR NAMES lzo1x.h
+                             PATH_SUFFIXES lzo
+                             NO_CACHE)
 
-find_library(LZO2_LIBRARY NAMES lzo2 liblzo2)
+  find_library(LZO2_LIBRARY NAMES lzo2 liblzo2
+                            NO_CACHE)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Lzo2
-                                  REQUIRED_VARS LZO2_LIBRARY LZO2_INCLUDE_DIR)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Lzo2
+                                    REQUIRED_VARS LZO2_LIBRARY LZO2_INCLUDE_DIR)
 
-if(LZO2_FOUND)
-  set(LZO2_LIBRARIES ${LZO2_LIBRARY})
-  set(LZO2_INCLUDE_DIRS ${LZO2_INCLUDE_DIR})
-
-  if(NOT TARGET Lzo2::Lzo2)
+  if(LZO2_FOUND)
     add_library(Lzo2::Lzo2 UNKNOWN IMPORTED)
     set_target_properties(Lzo2::Lzo2 PROPERTIES
                                      IMPORTED_LOCATION "${LZO2_LIBRARY}"
                                      INTERFACE_INCLUDE_DIRECTORIES "${LZO2_INCLUDE_DIR}")
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP Lzo2::Lzo2)
   endif()
 endif()
-
-mark_as_advanced(LZO2_INCLUDE_DIR LZO2_LIBRARY)
