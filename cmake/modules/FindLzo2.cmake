f8591c5e2a878b82d558c07090d40912f474623e
fuzzard <fuzzard@kodi.tv>
2023-09-10 19:18:11 +1000

    [cmake] FindLzo2 handle release/debug variants for windows

--- cmake/modules/FindLzo2.cmake
+++ cmake/modules/FindLzo2.cmake
@@ -12,8 +12,13 @@ if(NOT TARGET Lzo2::Lzo2)
                              PATH_SUFFIXES lzo
                              NO_CACHE)
 
-  find_library(LZO2_LIBRARY NAMES lzo2 liblzo2
-                            NO_CACHE)
+  find_library(LZO2_LIBRARY_RELEASE NAMES lzo2 liblzo2
+                                    NO_CACHE)
+  find_library(LZO2_LIBRARY_DEBUG NAMES lzo2d liblzo2d
+                                  NO_CACHE)
+
+  include(SelectLibraryConfigurations)
+  select_library_configurations(LZO2)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Lzo2
@@ -22,8 +27,14 @@ if(NOT TARGET Lzo2::Lzo2)
   if(LZO2_FOUND)
     add_library(Lzo2::Lzo2 UNKNOWN IMPORTED)
     set_target_properties(Lzo2::Lzo2 PROPERTIES
-                                     IMPORTED_LOCATION "${LZO2_LIBRARY}"
                                      INTERFACE_INCLUDE_DIRECTORIES "${LZO2_INCLUDE_DIR}")
+    if(LZO2_LIBRARY_RELEASE)
+      set_target_properties(Lzo2::Lzo2 PROPERTIES
+                                       IMPORTED_LOCATION_RELEASE "${LZO2_LIBRARY_RELEASE}")
+    if(LZO2_LIBRARY_DEBUG)
+      set_target_properties(Lzo2::Lzo2 PROPERTIES
+                                       IMPORTED_LOCATION_DEBUG "${LZO2_LIBRARY_DEBUG}")
+    endif()
     set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP Lzo2::Lzo2)
   endif()
 endif()
