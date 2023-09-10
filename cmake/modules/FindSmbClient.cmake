cf950f227d6c4b7602a8c971ec2df0b65b9a9db1
fuzzard <fuzzard@kodi.tv>
2023-09-10 15:44:54 +1000

    [cmake] FindSmbClient migrate to full TARGET usage

--- cmake/modules/FindSmbClient.cmake
+++ cmake/modules/FindSmbClient.cmake
@@ -3,48 +3,49 @@
 # -------------
 # Finds the SMB Client library
 #
-# This will define the following variables::
-#
-# SMBCLIENT_FOUND - system has SmbClient
-# SMBCLIENT_INCLUDE_DIRS - the SmbClient include directory
-# SMBCLIENT_LIBRARIES - the SmbClient libraries
-# SMBCLIENT_DEFINITIONS - the SmbClient definitions
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   SmbClient::SmbClient   - The SmbClient library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_SMBCLIENT smbclient QUIET)
-endif()
+if(NOT TARGET SmbClient::SmbClient)
+  if(WIN32 OR WINDOWS_STORE)
+    set(SMBCLIENT_FOUND TRUE)
+  else()
+    find_package(PkgConfig)
+    if(PKG_CONFIG_FOUND)
+      pkg_check_modules(SMBCLIENT smbclient QUIET)
+    endif()
 
-find_path(SMBCLIENT_INCLUDE_DIR NAMES libsmbclient.h
-                                PATHS ${PC_SMBCLIENT_INCLUDEDIR})
-find_library(SMBCLIENT_LIBRARY NAMES smbclient
-                               PATHS ${PC_SMBCLIENT_LIBDIR})
+    find_path(SMBCLIENT_INCLUDE_DIR NAMES libsmbclient.h
+                                    PATHS ${SMBCLIENT_INCLUDEDIR}
+                                    NO_CACHE)
+    find_library(SMBCLIENT_LIBRARY NAMES smbclient
+                                   PATHS ${SMBCLIENT_LIBDIR}
+                                   NO_CACHE)
 
-set(SMBCLIENT_VERSION ${PC_SMBCLIENT_VERSION})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SmbClient
-                                  REQUIRED_VARS SMBCLIENT_LIBRARY SMBCLIENT_INCLUDE_DIR
-                                  VERSION_VAR SMBCLIENT_VERSION)
-
-if(SMBCLIENT_FOUND)
-  set(SMBCLIENT_LIBRARIES ${SMBCLIENT_LIBRARY})
-  if(${SMBCLIENT_LIBRARY} MATCHES ".+\.a$" AND PC_SMBCLIENT_STATIC_LIBRARIES)
-    list(APPEND SMBCLIENT_LIBRARIES ${PC_SMBCLIENT_STATIC_LIBRARIES})
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(SmbClient
+                                      REQUIRED_VARS SMBCLIENT_LIBRARY SMBCLIENT_INCLUDE_DIR
+                                      VERSION_VAR SMBCLIENT_VERSION)
   endif()
-  set(SMBCLIENT_INCLUDE_DIRS ${SMBCLIENT_INCLUDE_DIR})
-  set(SMBCLIENT_DEFINITIONS -DHAS_FILESYSTEM_SMB=1)
 
-  if(NOT TARGET SmbClient::SmbClient)
+  if(SMBCLIENT_FOUND)
     add_library(SmbClient::SmbClient UNKNOWN IMPORTED)
     set_target_properties(SmbClient::SmbClient PROPERTIES
-                                   IMPORTED_LOCATION "${SMBCLIENT_LIBRARY}"
-                                   INTERFACE_INCLUDE_DIRECTORIES "${SMBCLIENT_INCLUDE_DIR}"
-                                   INTERFACE_COMPILE_DEFINITIONS HAS_FILESYSTEM_SMB=1)
+                                               INTERFACE_COMPILE_DEFINITIONS HAS_FILESYSTEM_SMB=1)
+
+    if(SMBCLIENT_LIBRARY)
+      set_target_properties(SmbClient::SmbClient PROPERTIES
+                                                 IMPORTED_LOCATION "${SMBCLIENT_LIBRARY}"
+                                                 INTERFACE_INCLUDE_DIRECTORIES "${SMBCLIENT_INCLUDE_DIR}")
+    endif()
+
+    # if pkg-config returns link libs add to TARGET.
+    if(${SMBCLIENT_LIBRARY} MATCHES ".+\.a$" AND SMBCLIENT_STATIC_LIBRARIES)
+        set_target_properties(SmbClient::SmbClient PROPERTIES
+                                                   INTERFACE_LINK_LIBRARIES "${SMBCLIENT_STATIC_LIBRARIES}")
+    endif()
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP SmbClient::SmbClient)
   endif()
 endif()
-
-mark_as_advanced(LIBSMBCLIENT_INCLUDE_DIR LIBSMBCLIENT_LIBRARY)
