5012b98692634461f76f65436b71812bdf77331f
fuzzard <fuzzard@kodi.tv>
2023-09-21 08:15:09 +1000

    [cmake] FindCdio FindIso9660pp migrate to TARGET usage

--- cmake/modules/FindCdio.cmake
+++ cmake/modules/FindCdio.cmake
@@ -3,40 +3,55 @@
 # --------
 # Finds the cdio library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# CDIO_FOUND - system has cdio
-# CDIO_INCLUDE_DIRS - the cdio include directory
-# CDIO_LIBRARIES - the cdio libraries
+# CDIO::CDIO - the cdio libraries
+# CDIO::CDIOPP - the cdio++ library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_CDIO libcdio>=0.80 QUIET)
-  pkg_check_modules(PC_CDIOPP libcdio++>=2.1.0 QUIET)
+if(NOT TARGET CDIO::CDIO)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_CDIO libcdio>=0.80 QUIET)
+    pkg_check_modules(PC_CDIOPP libcdio++>=2.1.0 QUIET)
+  endif()
+
+  find_path(CDIO_INCLUDE_DIR NAMES cdio/cdio.h
+                             HINTS ${DEPENDS_PATH}/include ${PC_CDIO_INCLUDEDIR}
+                             NO_CACHE)
+
+  find_library(CDIO_LIBRARY NAMES cdio libcdio
+                            HINTS ${DEPENDS_PATH}/lib ${PC_CDIO_LIBDIR}
+                            NO_CACHE)
+
+  if(DEFINED PC_CDIO_VERSION AND DEFINED PC_CDIOPP_VERSION AND NOT "${PC_CDIO_VERSION}" VERSION_EQUAL "${PC_CDIOPP_VERSION}")
+    message(WARNING "Detected libcdio (${PC_CDIO_VERSION}) and libcdio++ (${PC_CDIOPP_VERSION}) version mismatch. libcdio++ will not be used.")
+  else()
+    find_path(CDIOPP_INCLUDE_DIR NAMES cdio++/cdio.hpp
+                                 HINTS ${DEPENDS_PATH}/include ${PC_CDIOPP_INCLUDEDIR} ${CDIO_INCLUDE_DIR}
+                                 NO_CACHE)
+
+    set(CDIO_VERSION ${PC_CDIO_VERSION})
+  endif()
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Cdio
+                                    REQUIRED_VARS CDIO_LIBRARY CDIO_INCLUDE_DIR
+                                    VERSION_VAR CDIO_VERSION)
+
+  if(CDIO_FOUND)
+    add_library(CDIO::CDIO UNKNOWN IMPORTED)
+    set_target_properties(CDIO::CDIO PROPERTIES
+                                     IMPORTED_LOCATION "${CDIO_LIBRARY}"
+                                     INTERFACE_INCLUDE_DIRECTORIES "${CDIO_INCLUDE_DIR}")
+
+    if(CDIOPP_INCLUDE_DIR)
+      add_library(CDIO::CDIOPP INTERFACE IMPORTED)
+      set_target_properties(CDIO::CDIOPP PROPERTIES
+                                         INTERFACE_INCLUDE_DIRECTORIES "${CDIOPP_INCLUDE_DIR}")
+      target_link_libraries(CDIO::CDIO INTERFACE CDIO::CDIOPP)
+    endif()
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP CDIO::CDIO)
+  endif()
 endif()
-
-find_path(CDIO_INCLUDE_DIR NAMES cdio/cdio.h
-                           PATHS ${PC_CDIO_INCLUDEDIR})
-
-find_library(CDIO_LIBRARY NAMES cdio libcdio
-                          PATHS ${PC_CDIO_LIBDIR})
-
-if(DEFINED PC_CDIO_VERSION AND DEFINED PC_CDIOPP_VERSION AND NOT "${PC_CDIO_VERSION}" VERSION_EQUAL "${PC_CDIOPP_VERSION}")
-  message(WARNING "Detected libcdio (${PC_CDIO_VERSION}) and libcdio++ (${PC_CDIOPP_VERSION}) version mismatch. libcdio++ will not be used.")
-else()
-  find_path(CDIOPP_INCLUDE_DIR NAMES cdio++/cdio.hpp
-                               PATHS ${PC_CDIOPP_INCLUDEDIR} ${CDIO_INCLUDE_DIR})
-
-  set(CDIO_VERSION ${PC_CDIO_VERSION})
-endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cdio
-                                  REQUIRED_VARS CDIO_LIBRARY CDIO_INCLUDE_DIR
-                                  VERSION_VAR CDIO_VERSION)
-
-if(CDIO_FOUND)
-  set(CDIO_LIBRARIES ${CDIO_LIBRARY})
-  set(CDIO_INCLUDE_DIRS ${CDIO_INCLUDE_DIR})
-endif()
-
-mark_as_advanced(CDIO_INCLUDE_DIR CDIOPP_INCLUDE_DIR CDIO_LIBRARY)
--- cmake/modules/FindIso9660pp.cmake
+++ cmake/modules/FindIso9660pp.cmake
@@ -3,43 +3,65 @@
 # --------
 # Finds the iso9660++ library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# ISO9660PP_FOUND - system has iso9660++
-# ISO9660PP_INCLUDE_DIRS - the iso9660++ include directory
-# ISO9660PP_LIBRARIES - the iso9660++ libraries
-# ISO9660PP_DEFINITIONS  - the iso9660++ definitions
+# ISO9660::ISO9660 - the iso9660 libraries
+# ISO9660::ISO9660PP - the iso9660++ library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_ISO9660PP libiso9660++>=2.1.0 QUIET)
-  pkg_check_modules(PC_ISO9660 libiso9660>=2.1.0 QUIET)
+if(NOT TARGET ISO9660::ISO9660PP)
+  if(Iso9660pp_FIND_VERSION)
+    if(Iso9660pp_FIND_VERSION_EXACT)
+      set(Iso9660pp_FIND_SPEC "=${Iso9660pp_FIND_VERSION_COMPLETE}")
+    else()
+      set(Iso9660pp_FIND_SPEC ">=${Iso9660pp_FIND_VERSION_COMPLETE}")
+    endif()
+  endif()
+  find_package(PkgConfig)
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_ISO9660PP libiso9660++${Iso9660pp_FIND_SPEC} QUIET)
+    pkg_check_modules(PC_ISO9660 libiso9660${Iso9660pp_FIND_SPEC} QUIET)
+  endif()
+
+  find_package(Cdio REQUIRED)
+
+  find_path(ISO9660_INCLUDE_DIR NAMES iso9660.h
+                                PATH_SUFFIXES cdio
+                                HINTS ${DEPENDS_PATH}/include ${PC_ISO9660_INCLUDEDIR}
+                                NO_CACHE)
+
+  find_library(ISO9660_LIBRARY NAMES libiso9660 iso9660
+                               HINTS ${DEPENDS_PATH}/lib ${PC_ISO9660_LIBDIR}
+                               NO_CACHE)
+
+  find_path(ISO9660PP_INCLUDE_DIR NAMES iso9660.hpp
+                                  PATH_SUFFIXES cdio++
+                                  HINTS ${DEPENDS_PATH}/include ${PC_ISO9660PP_INCLUDEDIR}
+                                  NO_CACHE)
+
+  find_library(ISO9660PP_LIBRARY NAMES libiso9660++ iso9660++
+                                 HINTS ${DEPENDS_PATH}/lib ${PC_ISO9660PP_LIBDIR}
+                                 NO_CACHE)
+
+  set(ISO9660PP_VERSION ${PC_ISO9660PP_VERSION})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Iso9660pp
+                                    REQUIRED_VARS ISO9660PP_LIBRARY ISO9660PP_INCLUDE_DIR ISO9660_LIBRARY ISO9660_INCLUDE_DIR
+                                    VERSION_VAR ISO9660PP_VERSION)
+
+  if(ISO9660PP_FOUND)
+    add_library(ISO9660::ISO9660 UNKNOWN IMPORTED)
+    set_target_properties(ISO9660::ISO9660 PROPERTIES
+                                           IMPORTED_LOCATION "${ISO9660_LIBRARY}"
+                                           INTERFACE_INCLUDE_DIRECTORIES "${ISO9660_INCLUDE_DIR}")
+
+    add_library(ISO9660::ISO9660PP UNKNOWN IMPORTED)
+    set_target_properties(ISO9660::ISO9660PP PROPERTIES
+                                             IMPORTED_LOCATION "${ISO9660PP_LIBRARY}"
+                                             INTERFACE_INCLUDE_DIRECTORIES "${ISO9660PP_INCLUDE_DIR}"
+                                             INTERFACE_COMPILE_DEFINITIONS HAS_ISO9660PP=1
+                                             INTERFACE_LINK_LIBRARIES "ISO9660::ISO9660;CDIO::CDIO")
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP ISO9660::ISO9660PP)
+  endif()
 endif()
-
-find_package(Cdio)
-
-find_path(ISO9660PP_INCLUDE_DIR NAMES cdio++/iso9660.hpp
-                                PATHS ${PC_ISO9660PP_INCLUDEDIR})
-
-find_library(ISO9660PP_LIBRARY NAMES libiso9660++ iso9660++
-                               PATHS ${PC_ISO9660PP_LIBDIR})
-
-find_path(ISO9660_INCLUDE_DIR NAMES cdio/iso9660.h
-                              PATHS ${PC_ISO9660_INCLUDEDIR})
-
-find_library(ISO9660_LIBRARY NAMES libiso9660 iso9660
-                             PATHS ${PC_ISO9660_LIBDIR})
-
-set(ISO9660PP_VERSION ${PC_ISO9660PP_VERSION})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Iso9660pp
-                                  REQUIRED_VARS ISO9660PP_LIBRARY ISO9660PP_INCLUDE_DIR ISO9660_LIBRARY ISO9660_INCLUDE_DIR CDIO_LIBRARY CDIO_INCLUDE_DIR CDIOPP_INCLUDE_DIR
-                                  VERSION_VAR ISO9660PP_VERSION)
-
-if(ISO9660PP_FOUND)
-  set(ISO9660PP_LIBRARIES ${ISO9660PP_LIBRARY} ${ISO9660_LIBRARY} ${CDIO_LIBRARY})
-  set(ISO9660PP_INCLUDE_DIRS ${CDIO_INCLUDE_DIR} ${CDIOPP_INCLUDE_DIR} ${ISO9660_INCLUDE_DIR} ${ISO9660PP_INCLUDE_DIR})
-  set(ISO9660PP_DEFINITIONS -DHAS_ISO9660PP=1)
-endif()
-
-mark_as_advanced(ISO9660PP_INCLUDE_DIR ISO9660PP_LIBRARY ISO9660_INCLUDE_DIR ISO9660_LIBRARY)
