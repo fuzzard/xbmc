2775480c1cc0c4963a756acaac80f8e3d063768b
fuzzard <fuzzard@kodi.tv>
2023-09-21 14:33:11 +1000

    [cmake] FindCurl migrate to TARGET usage

--- cmake/modules/FindCurl.cmake
+++ cmake/modules/FindCurl.cmake
@@ -3,60 +3,43 @@
 # --------
 # Finds the Curl library
 #
-# This will define the following variables::
-#
-# CURL_FOUND - system has Curl
-# CURL_INCLUDE_DIRS - the Curl include directory
-# CURL_LIBRARIES - the Curl libraries
-# CURL_DEFINITIONS - the Curl definitions
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   Curl::Curl   - The Curl library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_CURL libcurl QUIET)
-endif()
+if(NOT TARGET Curl::Curl)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_CURL libcurl QUIET IMPORTED_TARGET)
+  endif()
 
-find_path(CURL_INCLUDE_DIR NAMES curl/curl.h
-                           PATHS ${PC_CURL_INCLUDEDIR})
-find_library(CURL_LIBRARY NAMES curl libcurl libcurl_imp
-                          PATHS ${PC_CURL_LIBDIR})
+  find_path(CURL_INCLUDE_DIR NAMES curl/curl.h
+                             HINTS ${DEPENDS_PATH}/include ${PC_CURL_INCLUDEDIR}
+                             ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
+                             NO_CACHE)
+  find_library(CURL_LIBRARY NAMES curl libcurl libcurl_imp
+                            HINTS ${DEPENDS_PATH}/lib ${PC_CURL_LIBDIR}
+                            ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
+                            NO_CACHE)
 
-set(CURL_VERSION ${PC_CURL_VERSION})
+  set(CURL_VERSION ${PC_CURL_VERSION})
 
-set(CURL_LIB_TYPE SHARED)
-set(CURL_LDFLAGS ${PC_CURL_LDFLAGS})
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Curl
+                                    REQUIRED_VARS CURL_LIBRARY CURL_INCLUDE_DIR
+                                    VERSION_VAR CURL_VERSION)
 
-# check if curl is statically linked
-if(${CURL_LIBRARY} MATCHES ".+\.a$" AND PC_CURL_STATIC_LDFLAGS)
-  set(CURL_LIB_TYPE STATIC)
-  set(CURL_LDFLAGS ${PC_CURL_STATIC_LDFLAGS})
-
-  pkg_check_modules(PC_NGHTTP2 libnghttp2 QUIET)
-  find_library(NGHTTP2_LIBRARY NAMES libnghttp2 nghttp2
-                               PATHS ${PC_NGHTTP2_LIBDIR})
-endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Curl
-                                  REQUIRED_VARS CURL_LIBRARY CURL_INCLUDE_DIR
-                                  VERSION_VAR CURL_VERSION)
-
-if(CURL_FOUND)
-  set(CURL_INCLUDE_DIRS ${CURL_INCLUDE_DIR})
-  set(CURL_LIBRARIES ${CURL_LIBRARY} ${NGHTTP2_LIBRARY})
-
-  if(NOT TARGET Curl::Curl)
-    add_library(Curl::Curl ${CURL_LIB_TYPE} IMPORTED)
-    set_target_properties(Curl::Curl PROPERTIES
-                                     IMPORTED_LOCATION "${CURL_LIBRARY}"
-                                     INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIR}")
-    if(HAS_CURL_STATIC)
-        set_target_properties(Curl::Curl PROPERTIES
-                                         INTERFACE_COMPILE_DEFINITIONS HAS_CURL_STATIC=1)
+  if(CURL_FOUND)
+    if(TARGET PkgConfig::PC_CURL)
+      add_library(Curl::Curl ALIAS PkgConfig::PC_CURL)
+    else()
+      add_library(Curl::Curl UNKNOWN IMPORTED)
+      set_target_properties(Curl::Curl PROPERTIES
+                                       IMPORTED_LOCATION "${CURL_LIBRARY}"
+                                       INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIR}")
     endif()
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP Curl::Curl)
   endif()
 endif()
-
-mark_as_advanced(CURL_INCLUDE_DIR CURL_LIBRARY CURL_LDFLAGS)
