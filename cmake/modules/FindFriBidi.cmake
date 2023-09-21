859717f491fa32960e255baf77e7ba7a744476f9
fuzzard <fuzzard@kodi.tv>
2023-09-21 15:08:45 +1000

    [cmake] FindFriBidi migrate to TARGET usage

--- cmake/modules/FindFriBidi.cmake
+++ cmake/modules/FindFriBidi.cmake
@@ -3,50 +3,44 @@
 # -----------
 # Finds the GNU FriBidi library
 #
-# This will define the following variables::
-#
-# FRIBIDI_FOUND - system has FriBidi
-# FRIBIDI_INCLUDE_DIRS - the FriBidi include directory
-# FRIBIDI_LIBRARIES - the FriBidi libraries
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   FriBidi::FriBidi   - The FriBidi library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_FRIBIDI fribidi QUIET)
+if(NOT TARGET FriBidi::FriBidi)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_FRIBIDI fribidi QUIET IMPORTED_TARGET)
+  endif()
+
+  find_path(FRIBIDI_INCLUDE_DIR NAMES fribidi.h
+                                PATH_SUFFIXES fribidi
+                                HINTS ${DEPENDS_PATH}/include ${PC_FRIBIDI_INCLUDEDIR}
+                                ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
+                                NO_CACHE)
+  find_library(FRIBIDI_LIBRARY NAMES fribidi libfribidi
+                               HINTS ${DEPENDS_PATH}/lib ${PC_FRIBIDI_LIBDIR}
+                               ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
+                               NO_CACHE)
+
+  set(FRIBIDI_VERSION ${PC_FRIBIDI_VERSION})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(FriBidi
+                                    REQUIRED_VARS FRIBIDI_LIBRARY FRIBIDI_INCLUDE_DIR
+                                    VERSION_VAR FRIBIDI_VERSION)
+
+  if(FRIBIDI_FOUND)
+    if(TARGET PkgConfig::PC_FRIBIDI)
+      add_library(FriBidi::FriBidi ALIAS PkgConfig::PC_FRIBIDI)
+    else()
+      add_library(FriBidi::FriBidi UNKNOWN IMPORTED)
+      set_target_properties(FriBidi::FriBidi PROPERTIES
+                                             IMPORTED_LOCATION "${FRIBIDI_LIBRARY}"
+                                             INTERFACE_INCLUDE_DIRECTORIES "${FRIBIDI_INCLUDE_DIR}")
+    endif()
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP FriBidi::FriBidi)
+  endif()
 endif()
-
-find_path(FRIBIDI_INCLUDE_DIR NAMES fribidi.h
-                              PATH_SUFFIXES fribidi
-                              PATHS ${PC_FRIBIDI_INCLUDEDIR})
-find_library(FRIBIDI_LIBRARY NAMES fribidi libfribidi
-                             PATHS ${PC_FRIBIDI_LIBDIR})
-
-set(FRIBIDI_VERSION ${PC_FRIBIDI_VERSION})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FriBidi
-                                  REQUIRED_VARS FRIBIDI_LIBRARY FRIBIDI_INCLUDE_DIR
-                                  VERSION_VAR FRIBIDI_VERSION)
-
-if(FRIBIDI_FOUND)
-  set(FRIBIDI_LIBRARIES ${FRIBIDI_LIBRARY})
-  set(FRIBIDI_INCLUDE_DIRS ${FRIBIDI_INCLUDE_DIR})
-  if(PC_FRIBIDI_INCLUDE_DIRS)
-    list(APPEND FRIBIDI_INCLUDE_DIRS ${PC_FRIBIDI_INCLUDE_DIRS})
-  endif()
-  if(PC_FRIBIDI_CFLAGS_OTHER)
-    set(FRIBIDI_DEFINITIONS ${PC_FRIBIDI_CFLAGS_OTHER})
-  endif()
-
-  if(NOT TARGET FriBidi::FriBidi)
-    add_library(FriBidi::FriBidi UNKNOWN IMPORTED)
-    set_target_properties(FriBidi::FriBidi PROPERTIES
-                                           IMPORTED_LOCATION "${FRIBIDI_LIBRARY}"
-                                           INTERFACE_INCLUDE_DIRECTORIES "${FRIBIDI_INCLUDE_DIRS}"
-                                           INTERFACE_COMPILE_OPTIONS "${FRIBIDI_DEFINITIONS}")
-  endif()
-endif()
-
-mark_as_advanced(FRIBIDI_INCLUDE_DIR FRIBIDI_LIBRARY)
