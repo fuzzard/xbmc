cb5b06ffc5494fd40a977a7f99c71fcd01b62e03
fuzzard <fuzzard@kodi.tv>
2023-09-21 15:26:31 +1000

    [cmake] FindGLX migrate to TARGET usage

--- cmake/modules/FindGLX.cmake
+++ cmake/modules/FindGLX.cmake
@@ -3,34 +3,35 @@
 # -----
 # Finds the GLX library
 #
-# This will define the following variables::
-#
-# GLX_FOUND - system has GLX
-# GLX_INCLUDE_DIRS - the GLX include directory
-# GLX_LIBRARIES - the GLX libraries
-# GLX_DEFINITIONS - the GLX definitions
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   GLX::GLX    - The GLX library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_GLX glx QUIET)
+if(NOT TARGET GLX::GLX)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_GLX glx QUIET)
+  endif()
+
+  find_path(GLX_INCLUDE_DIR NAMES GL/glx.h
+                            HINTS ${PC_GLX_INCLUDEDIR}
+                            NO_CACHE)
+  find_library(GLX_LIBRARY NAMES GL
+                           HINTS ${PC_GLX_LIBDIR}
+                           NO_CACHE)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(GLX
+                                    REQUIRED_VARS GLX_LIBRARY GLX_INCLUDE_DIR)
+
+  if(GLX_FOUND)
+    add_library(GLX::GLX UNKNOWN IMPORTED)
+    set_target_properties(GLX::GLX PROPERTIES
+                                   IMPORTED_LOCATION "${GLX_LIBRARY}"
+                                   INTERFACE_INCLUDE_DIRECTORIES "${GLX_INCLUDE_DIR}"
+                                   INTERFACE_COMPILE_DEFINITIONS HAS_GLX=1)
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP GLX::GLX)
+  endif()
 endif()
-
-find_path(GLX_INCLUDE_DIR NAMES GL/glx.h
-                          PATHS ${PC_GLX_INCLUDEDIR})
-find_library(GLX_LIBRARY NAMES GL
-                         PATHS ${PC_GLX_LIBDIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLX
-                                  REQUIRED_VARS GLX_LIBRARY GLX_INCLUDE_DIR)
-
-if(GLX_FOUND)
-  set(GLX_LIBRARIES ${GLX_LIBRARY})
-  set(GLX_INCLUDE_DIRS ${GLX_INCLUDE_DIR})
-  set(GLX_DEFINITIONS -DHAS_GLX=1)
-endif()
-
-mark_as_advanced(GLX_INCLUDE_DIR GLX_LIBRARY)
