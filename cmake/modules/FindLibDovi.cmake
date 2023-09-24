f21d98905c9d51f587dbfaf2e5c28dfe8cfa2241
fuzzard <fuzzard@kodi.tv>
2023-09-24 19:46:08 +1000

    [cmake] FindLibDovi migrate to TARGET usage

--- cmake/modules/FindLibDovi.cmake
+++ cmake/modules/FindLibDovi.cmake
@@ -2,31 +2,37 @@
 # -------
 # Finds the libdovi library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# LIBDOVI_FOUND - system has libdovi
-# LIBDOVI_INCLUDE_DIRS - the libdovi include directories
-# LIBDOVI_LIBRARIES - the libdovi libraries
-# LIBDOVI_DEFINITIONS - the libdovi compile definitions
+#   libdovi::libdovi - The libdovi library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_LIBDOVI libdovi QUIET)
+if(NOT TARGET libdovi::libdovi)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_LIBDOVI libdovi QUIET)
+  endif()
+
+  find_library(LIBDOVI_LIBRARY NAMES dovi libdovi
+                               HINTS ${DEPENDS_PATH}/lib ${PC_LIBDOVI_LIBDIR}
+                               ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
+                               NO_CACHE)
+  find_path(LIBDOVI_INCLUDE_DIR NAMES libdovi/rpu_parser.h
+                                HINTS ${DEPENDS_PATH}/include ${PC_LIBDOVI_INCLUDEDIR}
+                                ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
+                                NO_CACHE)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LibDovi
+                                    REQUIRED_VARS LIBDOVI_LIBRARY LIBDOVI_INCLUDE_DIR)
+
+  if(LIBDOVI_FOUND)
+    add_library(libdovi::libdovi UNKNOWN IMPORTED)
+    set_target_properties(libdovi::libdovi PROPERTIES
+                                             IMPORTED_LOCATION "${LIBDOVI_LIBRARY}"
+                                             INTERFACE_INCLUDE_DIRECTORIES "${LIBDOVI_INCLUDE_DIR}"
+                                             INTERFACE_COMPILE_DEFINITIONS HAVE_LIBDOVI=1)
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libdovi::libdovi)
+  endif()
 endif()
-
-find_library(LIBDOVI_LIBRARY NAMES dovi libdovi
-                             PATHS ${PC_LIBDOVI_LIBDIR}
-)
-find_path(LIBDOVI_INCLUDE_DIR NAMES libdovi/rpu_parser.h
-                              PATHS ${PC_LIBDOVI_INCLUDEDIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibDovi
-                                  REQUIRED_VARS LIBDOVI_LIBRARY LIBDOVI_INCLUDE_DIR)
-
-if(LIBDOVI_FOUND)
-  set(LIBDOVI_INCLUDE_DIRS ${LIBDOVI_INCLUDE_DIR})
-  set(LIBDOVI_LIBRARIES ${LIBDOVI_LIBRARY})
-  set(LIBDOVI_DEFINITIONS -DHAVE_LIBDOVI=1)
-endif()
-
-mark_as_advanced(LIBDOVI_INCLUDE_DIR LIBDOVI_LIBRARY)
