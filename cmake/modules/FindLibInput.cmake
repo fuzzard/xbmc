10a1896309d34be49501a8440c0bb2a45c892d6a
fuzzard <fuzzard@kodi.tv>
2023-09-24 19:46:44 +1000

    [cmake] FindLibInput migrate to TARGET usage

--- cmake/modules/FindLibInput.cmake
+++ cmake/modules/FindLibInput.cmake
@@ -3,34 +3,38 @@
 # --------
 # Finds the libinput library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# LIBINPUT_FOUND - system has libinput
-# LIBINPUT_INCLUDE_DIRS - the libinput include directory
-# LIBINPUT_LIBRARIES - the libinput libraries
-# LIBINPUT_DEFINITIONS - the libinput compile definitions
-#
+#   libinput::libinput - The libinput library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_LIBINPUT libinput QUIET)
+if(NOT TARGET libinput::libinput)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_LIBINPUT libinput QUIET)
+  endif()
+
+  find_path(LIBINPUT_INCLUDE_DIR NAMES libinput.h
+                                 HINTS ${PC_LIBINPUT_INCLUDEDIR}
+                                 NO_CACHE)
+
+  find_library(LIBINPUT_LIBRARY NAMES input
+                                HINTS ${PC_LIBINPUT_LIBDIR}
+                                NO_CACHE)
+
+  set(LIBINPUT_VERSION ${PC_LIBINPUT_VERSION})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LibInput
+                                    REQUIRED_VARS LIBINPUT_LIBRARY LIBINPUT_INCLUDE_DIR
+                                    VERSION_VAR LIBINPUT_VERSION)
+
+  if(LIBINPUT_FOUND)
+    add_library(libinput::libinput UNKNOWN IMPORTED)
+    set_target_properties(libinput::libinput PROPERTIES
+                                             IMPORTED_LOCATION "${LIBINPUT_LIBRARY}"
+                                             INTERFACE_INCLUDE_DIRECTORIES "${LIBINPUT_INCLUDE_DIR}")
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libinput::libinput)
+  endif()
 endif()
-
-find_path(LIBINPUT_INCLUDE_DIR NAMES libinput.h
-                               PATHS ${PC_LIBINPUT_INCLUDEDIR})
-
-find_library(LIBINPUT_LIBRARY NAMES input
-                              PATHS ${PC_LIBINPUT_LIBDIR})
-
-set(LIBINPUT_VERSION ${PC_LIBINPUT_VERSION})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibInput
-                                  REQUIRED_VARS LIBINPUT_LIBRARY LIBINPUT_INCLUDE_DIR
-                                  VERSION_VAR LIBINPUT_VERSION)
-
-if(LIBINPUT_FOUND)
-  set(LIBINPUT_INCLUDE_DIRS ${LIBINPUT_INCLUDE_DIR})
-  set(LIBINPUT_LIBRARIES ${LIBINPUT_LIBRARY})
-endif()
-
-mark_as_advanced(LIBINPUT_INCLUDE_DIR LIBINPUT_LIBRARY)
