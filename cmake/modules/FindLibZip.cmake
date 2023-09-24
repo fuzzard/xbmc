cd2c6d70390e865c32b72c4d766b088a1d9dfb8f
fuzzard <fuzzard@kodi.tv>
2023-09-24 19:42:41 +1000

    [cmake] FindLibZip migrate to TARGET usage

--- cmake/modules/FindLibZip.cmake
+++ cmake/modules/FindLibZip.cmake
@@ -13,74 +13,73 @@
 #
 #   libzip::zip - The LibZip library
 
-include(cmake/scripts/common/ModuleHelpers.cmake)
+if(NOT TARGET libzip::zip)
+  include(cmake/scripts/common/ModuleHelpers.cmake)
 
-set(MODULE_LC libzip)
-SETUP_BUILD_VARS()
+  set(MODULE_LC libzip)
+  SETUP_BUILD_VARS()
 
-# Check for existing lib
-find_package(LIBZIP CONFIG QUIET
-                    HINTS ${DEPENDS_PATH}/lib
-                    ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
+  # Check for existing lib
+  find_package(LIBZIP CONFIG QUIET
+                      HINTS ${DEPENDS_PATH}/lib/cmake
+                      ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
 
-if(NOT LIBZIP_FOUND OR LIBZIP_VERSION VERSION_LESS ${${MODULE}_VER})
-  # Check for dependencies
-  find_package(GnuTLS MODULE REQUIRED)
+  if(NOT LIBZIP_FOUND OR LIBZIP_VERSION VERSION_LESS ${${MODULE}_VER})
+    # Check for dependencies
+    find_package(GnuTLS MODULE REQUIRED)
 
-  # Eventually we will want Find modules for the following deps
-  # bzip2 
-  # ZLIB
+    # Eventually we will want Find modules for the following deps
+    # bzip2 
+    # ZLIB
 
-  set(CMAKE_ARGS -DBUILD_DOC=OFF
-                 -DBUILD_EXAMPLES=OFF
-                 -DBUILD_REGRESS=OFF
-                 -DBUILD_SHARED_LIBS=OFF
-                 -DBUILD_TOOLS=OFF)
+    set(CMAKE_ARGS -DBUILD_DOC=OFF
+                   -DBUILD_EXAMPLES=OFF
+                   -DBUILD_REGRESS=OFF
+                   -DBUILD_SHARED_LIBS=OFF
+                   -DBUILD_TOOLS=OFF)
 
-  set(LIBZIP_VERSION ${${MODULE}_VER})
+    set(LIBZIP_VERSION ${${MODULE}_VER})
 
-  BUILD_DEP_TARGET()
-else()
-  find_path(LIBZIP_INCLUDE_DIR NAMES zip.h
-                               HINTS ${DEPENDS_PATH}/include
-                               ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
-
-  find_library(LIBZIP_LIBRARY NAMES zip
-                              HINTS ${DEPENDS_PATH}/lib
-                              ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
-endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibZip
-                                  REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR
-                                  VERSION_VAR LIBZIP_VERSION)
-
-if(LIBZIP_FOUND)
-  set(LIBZIP_LIBRARIES ${LIBZIP_LIBRARY})
-  set(LIBZIP_INCLUDE_DIRS ${LIBZIP_INCLUDE_DIR})
-
-  if(NOT TARGET libzip::zip)
-    add_library(libzip::zip UNKNOWN IMPORTED)
-
-    set_target_properties(libzip::zip PROPERTIES
-                                         INTERFACE_INCLUDE_DIRECTORIES "${LIBZIP_INCLUDE_DIR}"
-                                         IMPORTED_LOCATION "${LIBZIP_LIBRARY}")
-
-    if(TARGET libzip)
-      add_dependencies(libzip::zip libzip)
-    endif()
+    BUILD_DEP_TARGET()
   else()
-    # ToDo: When we correctly import dependencies cmake targets for the following
-    # BZip2::BZip2, LibLZMA::LibLZMA, GnuTLS::GnuTLS, Nettle::Nettle,ZLIB::ZLIB
-    # For now, we just override 
-    set_target_properties(libzip::zip PROPERTIES
-                                      INTERFACE_LINK_LIBRARIES "")
+    find_path(LIBZIP_INCLUDE_DIR NAMES zip.h
+                                 HINTS ${DEPENDS_PATH}/include
+                                 ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG}
+                                 NO_CACHE)
+
+    find_library(LIBZIP_LIBRARY NAMES zip
+                                HINTS ${DEPENDS_PATH}/lib
+                                ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG}
+                                NO_CACHE)
   endif()
-  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libzip::zip)
-else()
-  if(LIBZIP_FIND_REQUIRED)
-    message(FATAL_ERROR "LibZip not found.")
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LibZip
+                                    REQUIRED_VARS LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR
+                                    VERSION_VAR LIBZIP_VERSION)
+
+  if(LIBZIP_FOUND)
+    if(NOT TARGET libzip::zip)
+      add_library(libzip::zip UNKNOWN IMPORTED)
+
+      set_target_properties(libzip::zip PROPERTIES
+                                        INTERFACE_INCLUDE_DIRECTORIES "${LIBZIP_INCLUDE_DIR}"
+                                        IMPORTED_LOCATION "${LIBZIP_LIBRARY}")
+
+      if(TARGET libzip)
+        add_dependencies(libzip::zip libzip)
+      endif()
+    else()
+      # ToDo: When we correctly import dependencies cmake targets for the following
+      # BZip2::BZip2, LibLZMA::LibLZMA, GnuTLS::GnuTLS, Nettle::Nettle,ZLIB::ZLIB
+      # For now, we just override
+      set_target_properties(libzip::zip PROPERTIES
+                                        INTERFACE_LINK_LIBRARIES "")
+    endif()
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP libzip::zip)
+  else()
+    if(LIBZIP_FIND_REQUIRED)
+      message(FATAL_ERROR "LibZip not found.")
+    endif()
   endif()
 endif()
-
-mark_as_advanced(LIBZIP_INCLUDE_DIR LIBZIP_LIBRARY)
