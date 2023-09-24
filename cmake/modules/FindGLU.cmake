61b8940f49767241f230e2bd7251b7896e760d16
fuzzard <fuzzard@kodi.tv>
2023-09-24 19:34:33 +1000

    [cmake] FindGLU migrate to TARGET usage

--- cmake/modules/FindGLU.cmake
+++ cmake/modules/FindGLU.cmake
@@ -3,31 +3,39 @@
 # -----
 # Finds the GLU library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# GLU_FOUND - system has GLU
-# GLU_INCLUDE_DIRS - the GLU include directory
-# GLU_LIBRARIES - the GLU libraries
-# GLU_DEFINITIONS - the GLU definitions
-#
+#   OpenGL::GLU   - The GLU library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_GLU glu QUIET)
+if(NOT TARGET OpenGL::GLU)
+  find_package(PkgConfig)
+  # Do not use pkgconfig on windows
+  if(PKG_CONFIG_FOUND AND NOT WIN32)
+    pkg_check_modules(PC_GLU glu QUIET)
+  endif()
+
+  find_path(GLU_INCLUDE_DIR NAMES GL/glu.h
+                            HINTS ${PC_GLU_INCLUDEDIR}
+                            NO_CACHE)
+  find_library(GLU_LIBRARY NAMES GLU
+                           HINTS ${PC_GLU_LIBDIR}
+                           NO_CACHE)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(GLU
+                                    REQUIRED_VARS GLU_LIBRARY GLU_INCLUDE_DIR)
+
+  if(GLU_FOUND)
+    if(TARGET PkgConfig::PC_GLU)
+      add_library(OpenGL::GLU ALIAS PkgConfig::PC_GLU)
+    else()
+      add_library(OpenGL::GLU UNKNOWN IMPORTED)
+      set_target_properties(OpenGL::GLU PROPERTIES
+                                        IMPORTED_LOCATION "${GLU_LIBRARY}"
+                                        INTERFACE_INCLUDE_DIRECTORIES "${GLU_INCLUDE_DIR}"
+                                        INTERFACE_COMPILE_DEFINITIONS HAS_GLU=1)
+    endif()
+
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP OpenGL::GLU)
+  endif()
 endif()
-
-find_path(GLU_INCLUDE_DIR NAMES GL/glu.h
-                          PATHS ${PC_GLU_INCLUDEDIR})
-find_library(GLU_LIBRARY NAMES GLU
-                         PATHS ${PC_GLU_LIBDIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLU
-                                  REQUIRED_VARS GLU_LIBRARY GLU_INCLUDE_DIR)
-
-if(GLU_FOUND)
-  set(GLU_LIBRARIES ${GLU_LIBRARY})
-  set(GLU_INCLUDE_DIRS ${GLU_INCLUDE_DIR})
-  set(GLU_DEFINITIONS -DHAS_GLU=1)
-endif()
-
-mark_as_advanced(GLU_INCLUDE_DIR GLU_LIBRARY)
