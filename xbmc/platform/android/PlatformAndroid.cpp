7b1f44e519a78717b698993705b85c4c45a22ff3
fuzzard <fuzzard@kodi.tv>
2024-04-11 17:39:13 +1000

    WIp

--- xbmc/platform/android/PlatformAndroid.cpp
+++ xbmc/platform/android/PlatformAndroid.cpp
@@ -33,8 +33,6 @@ bool CPlatformAndroid::InitStageOne()
 
   setenv("OS", "Linux", true); // for python scripts that check the OS
 
-CLog::LogF(LOGDEBUG, "KODI_BINADDON_PATH: {}; KODI_ANDROID_APK: {}", getenv("KODI_BINADDON_PATH"), getenv("KODI_ANDROID_APK"));
-
   CWinSystemAndroidGLESContext::Register();
 
   CAndroidPowerSyscall::Register();
