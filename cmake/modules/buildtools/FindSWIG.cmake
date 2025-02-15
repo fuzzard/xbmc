559f9848508dc0b56cb361a84cd355e70c0533bb
fuzzard <fuzzard@kodi.tv>
2025-02-15 13:48:02 +1000

    [swig][cmake] Change to swig target update dep tracking for swig
    Implement TARGET usage for swig tool
    
    Our swig generation was only tracking the explicit swig template and the input template file
    This was discovered due to changes in https://github.com/xbmc/xbmc/pull/26191 that did not
    change the swig templates, but did change headers that the swig templates included.
    
    This now adds dependency tracking to any files included as part of the swig template, and
    also adds cmake reconfiguration tracking on those files to regenerate the cmake project if
    one is changes for any possible non-existing input data for swig (ie a new include in a
    template or included header)

--- cmake/modules/buildtools/FindSWIG.cmake
+++ cmake/modules/buildtools/FindSWIG.cmake
@@ -9,9 +9,16 @@
 
 if(NOT TARGET SWIG::SWIG)
   find_program(SWIG_EXECUTABLE NAMES swig4.0 swig3.0 swig2.0 swig
+                               NO_CACHE
                                PATH_SUFFIXES swig
                                HINTS ${NATIVEPREFIX}/bin)
-  if(SWIG_EXECUTABLE)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SWIG
+                                    REQUIRED_VARS SWIG_EXECUTABLE
+                                    VERSION_VAR SWIG_VERSION)
+
+  if(SWIG_FOUND)
     execute_process(COMMAND ${SWIG_EXECUTABLE} -version
       OUTPUT_VARIABLE SWIG_version_output
       ERROR_VARIABLE SWIG_version_output
@@ -23,10 +30,9 @@ if(NOT TARGET SWIG::SWIG)
     set_target_properties(SWIG::SWIG PROPERTIES
                                      IMPORTED_LOCATION "${SWIG_EXECUTABLE}"
                                      VERSION "${SWIG_VERSION}")
+  else()
+    if(SWIG_FIND_REQUIRED)
+      message(FATAL_ERROR "Swig executable was not found.")
+    endif()
   endif()
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(SWIG
-                                    REQUIRED_VARS SWIG_EXECUTABLE
-                                    VERSION_VAR SWIG_VERSION)
 endif()
