bd756431a729076249e6608d85a28ef8fd11060c
fuzzard <fuzzard@kodi.tv>
2025-03-23 10:03:42 +1000

    [cmake] Build internal libaacs

--- cmake/modules/FindLibAACS.cmake
+++ cmake/modules/FindLibAACS.cmake
@@ -9,41 +9,125 @@
 
 if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
 
-  find_package(PkgConfig ${SEARCH_QUIET})
+  macro(buildmacroLibAACS)
 
-  # We only rely on pkgconfig for non windows platforms
-  if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWS_STORE))
-    pkg_check_modules(LIBAACS libaacs IMPORTED_TARGET ${SEARCH_QUIET})
+    set(patches "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/01-all-remove_versioning.patch"
+                "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/02-all-AACS_HOME-env.patch")
 
-    get_target_property(LIBAACS_LIBRARY PkgConfig::LIBAACS INTERFACE_LINK_LIBRARIES)
-    get_target_property(LIBAACS_INCLUDE_DIR PkgConfig::LIBAACS INTERFACE_INCLUDE_DIRECTORIES)
+    if(CORE_SYSTEM_NAME MATCHES windows)
+      list(APPEND patches "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/03-win-cmake.patch"
+                          "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/04-win-UWP.patch"
+                          "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/05-win-fix_gcrypt_build.patch")
+    endif()
+
+    generate_patchcommand("${patches}")
+
+    if(CORE_SYSTEM_NAME MATCHES windows)
+      find_package(libgpg-error CONFIG REQUIRED ${SEARCH_QUIET})
+      find_package(libgcrypt CONFIG REQUIRED ${SEARCH_QUIET})
+      find_package(iconv CONFIG REQUIRED ${SEARCH_QUIET})
+
+      # Override build type detection and always build as release
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_TYPE Release)
+
+      set(CMAKE_ARGS -DNATIVEPREFIX=${NATIVEPREFIX})
+    else()
+
+      if(CMAKE_HOST_SYSTEM_NAME MATCHES "(Free|Net|Open)BSD")
+        find_program(MAKE_EXECUTABLE gmake)
+      endif()
+      find_program(MAKE_EXECUTABLE make REQUIRED)
+      find_program(AUTORECONF autoreconf REQUIRED)
+
+      pkg_check_modules(libgcrypt libgcrypt REQUIRED ${SEARCH_QUIET})
+      pkg_check_modules(gpg-error gpg-error REQUIRED ${SEARCH_QUIET})
+
+      set(CONFIGURE_COMMAND ./bootstrap
+                    COMMAND ./configure
+                            --prefix=${DEPENDS_PATH}
+                            --disable-static
+                            --exec-prefix=${DEPENDS_PATH})
+
+      set(BUILD_COMMAND ${MAKE_EXECUTABLE})
+      set(INSTALL_COMMAND ${MAKE_EXECUTABLE} install)
+      set(BUILD_IN_SOURCE 1)
+  
+      if(CORE_SYSTEM_NAME STREQUAL "osx")
+        set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LOCATION_POSTFIX "dylib")
+      endif()
+    endif()
+
+    BUILD_DEP_TARGET()
+
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER})
+  endmacro()
+
+  set(${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC libaacs)
+
+  SETUP_BUILD_VARS()
+
+  # Kodi libaacs-0.9.0-* prebuilt libs have a broken cmake config file
+  # We need to detect this, otherwise the call to find_package will run and error out
+  if(EXISTS ${DEPENDS_PATH}/lib/cmake/libaacs/libaacs-config.cmake)
+    file(READ ${DEPENDS_PATH}/lib/cmake/libaacs/libaacs-config.cmake aacs_config_output)
+    string(FIND ${aacs_config_output} "libbdplus.cmake" BROKEN_CONFIG)
   else()
-    # Current packaged windows libaacs cmake config has a fault, so we cant use find_package
-    find_path(LIBAACS_INCLUDE_DIR NAMES libaacs/aacs.h
-                                  HINTS ${DEPENDS_PATH}/include
-                                  ${${CORE_PLATFORM_LC}_SEARCH_CONFIG})
+    set(BROKEN_CONFIG -1)
+  endif()
 
-    find_library(LIBAACS_LIBRARY NAMES aacs libaacs
-                                 HINTS ${DEPENDS_PATH}/lib
-                                 ${${CORE_PLATFORM_LC}_SEARCH_CONFIG})
+  # Only do a package check if the broken config is not found
+  if(NOT (${BROKEN_CONFIG} GREATER "-1"))
+    find_package(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} ${CONFIG_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} CONFIG ${SEARCH_QUIET}
+                                                           HINTS ${DEPENDS_PATH}/lib/cmake
+                                                           ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
+  endif()
 
-    if(EXISTS ${LIBAACS_INCLUDEDIR}/libaacs/aacs-version.h)
-      file(STRINGS ${LIBAACS_INCLUDEDIR}/libaacs/aacs-version.h _aacs_version_str
-           REGEX "#define[ \t]AACS_VERSION_STRING[ \t][\"]?[0-9.]+[\"]?")
-      string(REGEX REPLACE "^.*AACS_VERSION_STRING[ \t][\"]?([0-9.]+).*$" "\\1" LIBAACS_VERSION ${_aacs_version_str})
-      unset(_aacs_version_str)
+  # fallback to pkgconfig for non windows platforms
+  if(NOT ${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_FOUND)
+    find_package(PkgConfig ${SEARCH_QUIET})
+
+    if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWSSTORE))
+      pkg_check_modules(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} ${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}${PC_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} ${SEARCH_QUIET} IMPORTED_TARGET)
+    endif()
+  endif()
+
+  if(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_VERSION VERSION_LESS ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER})
+    cmake_language(EVAL CODE "
+      buildmacro${CMAKE_FIND_PACKAGE_NAME}()
+    ")
+  else()
+    if(TARGET libaacs::libaacs)
+      get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY libaacs::libaacs IMPORTED_LOCATION_RELEASE)
+      get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR libaacs::libaacs INTERFACE_INCLUDE_DIRECTORIES)
+    elseif(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+      get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} INTERFACE_LINK_LIBRARIES)
+      get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} INTERFACE_INCLUDE_DIRECTORIES)
     endif()
   endif()
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(LibAACS
-                                    REQUIRED_VARS LIBAACS_LIBRARY LIBAACS_INCLUDE_DIR
-                                    VERSION_VAR LIBAACS_VERSION)
+                                    REQUIRED_VARS ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR
+                                    VERSION_VAR ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION)
 
-  if(LIBAACS_FOUND)
-    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
-    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
-                                                                     INTERFACE_INCLUDE_DIRECTORIES "${LIBAACS_INCLUDE_DIR}"
-                                                                     INTERFACE_LINK_LIBRARIES "${LIBAACS_LIBRARY}")
+  if(LibAACS_FOUND)
+    if(TARGET libaacs::libaacs AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS libaacs::libaacs)
+    elseif(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+    else()
+      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} INTERFACE IMPORTED)
+      set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                       INTERFACE_INCLUDE_DIRECTORIES "${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR}"
+                                                                       INTERFACE_LINK_LIBRARIES "${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY}")
+
+      add_dependencies(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+    endif()
+
+    ADD_MULTICONFIG_BUILDMACRO()
+  else()
+    if(LibAACS_FIND_REQUIRED)
+      message(FATAL_ERROR "libaacs libraries were not found.")
+    endif()
   endif()
 endif()
