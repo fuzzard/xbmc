f6dd3eb9b61087ad02bcfefcfc52ab07994b43cc
fuzzard <fuzzard@kodi.tv>
2025-05-05 10:33:42 +1000

    [cmake] FindBluray use new modulehelper macros

--- cmake/modules/FindBluray.cmake
+++ cmake/modules/FindBluray.cmake
@@ -13,73 +13,63 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
 
   set(${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC libbluray)
 
+  SETUP_BUILD_VARS()
+
   SETUP_FIND_SPECS()
 
-  # Check for existing libcec. If version >= LIBBLURAY-VERSION file version, dont build
-  find_package(libbluray ${CONFIG_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} CONFIG ${SEARCH_QUIET}
-                         HINTS ${DEPENDS_PATH}/lib/cmake
-                         ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
-
-  # cmake config may not be available (eg Debian libbluray-dev package)
-  # fallback to pkgconfig for non windows platforms
-  if(NOT libbluray_FOUND)
-    find_package(PkgConfig ${SEARCH_QUIET})
-    if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWSSTORE))
-      pkg_check_modules(libbluray libbluray${PC_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} ${SEARCH_QUIET} IMPORTED_TARGET)
-    endif()
-  endif()
+  SEARCH_EXISTING_PACKAGES()
 
   if(TARGET libbluray::libbluray)
-    get_target_property(_CEC_CONFIGURATIONS libbluray::libbluray IMPORTED_CONFIGURATIONS)
-    foreach(_libbluray_config IN LISTS _CEC_CONFIGURATIONS)
-      # Some non standard config (eg None on Debian)
-      # Just set to RELEASE var so select_library_configurations can continue to work its magic
-      string(TOUPPER ${_libbluray_config} _libbluray_config_UPPER)
-      if((NOT ${_libbluray_config_UPPER} STREQUAL "RELEASE") AND
-         (NOT ${_libbluray_config_UPPER} STREQUAL "DEBUG"))
-        get_target_property(BLURAY_LIBRARY_RELEASE libbluray::libbluray IMPORTED_LOCATION_${_libbluray_config_UPPER})
-      else()
-        get_target_property(BLURAY_LIBRARY_${_cec_config_UPPER} libbluray::libbluray IMPORTED_LOCATION_${_libbluray_config_UPPER})
-      endif()
-    endforeach()
+    get_target_property(_BLURAY_CONFIGURATIONS libbluray::libbluray IMPORTED_CONFIGURATIONS)
+    if(_BLURAY_CONFIGURATIONS)
+      foreach(_libbluray_config IN LISTS _BLURAY_CONFIGURATIONS)
+        # Some non standard config (eg None on Debian)
+        # Just set to RELEASE var so select_library_configurations can continue to work its magic
+        string(TOUPPER ${_libbluray_config} _libbluray_config_UPPER)
+        if((NOT ${_libbluray_config_UPPER} STREQUAL "RELEASE") AND
+           (NOT ${_libbluray_config_UPPER} STREQUAL "DEBUG"))
+          get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_RELEASE libbluray::libbluray IMPORTED_LOCATION_${_libbluray_config_UPPER})
+        else()
+          get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_${_cec_config_UPPER} libbluray::libbluray IMPORTED_LOCATION_${_libbluray_config_UPPER})
+        endif()
+      endforeach()
+    else()
+      get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_RELEASE libbluray::libbluray IMPORTED_LOCATION)
+    endif()
 
-    get_target_property(BLURAY_INCLUDE_DIR libbluray::libbluray INTERFACE_INCLUDE_DIRECTORIES)
-    set(BLURAY_VERSION ${libbluray_VERSION})
-  elseif(TARGET PkgConfig::libbluray)
+    get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR libbluray::libbluray INTERFACE_INCLUDE_DIRECTORIES)
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION ${libbluray_VERSION})
+  elseif(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
     # First item is the full path of the library file found
     # pkg_check_modules does not populate a variable of the found library explicitly
-    list(GET libbluray_LINK_LIBRARIES 0 BLURAY_LIBRARY_RELEASE)
-    get_target_property(BLURAY_INCLUDE_DIR PkgConfig::libbluray INTERFACE_INCLUDE_DIRECTORIES)
+    list(GET libbluray_LINK_LIBRARIES 0 ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_RELEASE)
+    get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} INTERFACE_INCLUDE_DIRECTORIES)
 
-    set(BLURAY_VERSION ${libbluray_VERSION})
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION ${libbluray_VERSION})
   endif()
 
   include(SelectLibraryConfigurations)
-  select_library_configurations(BLURAY)
-  unset(BLURAY_LIBRARIES)
+  select_library_configurations(${${CMAKE_FIND_PACKAGE_NAME}_MODULE})
+  unset(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARIES)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Bluray
-                                    REQUIRED_VARS BLURAY_LIBRARY BLURAY_INCLUDE_DIR
-                                    VERSION_VAR BLURAY_VERSION)
+                                    REQUIRED_VARS ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR
+                                    VERSION_VAR ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION)
 
   if(BLURAY_FOUND)
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_COMPILE_DEFINITIONS HAVE_LIBBLURAY)
     if(TARGET libbluray::libbluray)
       add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS libbluray::libbluray)
-      # We need to append in case the cmake config already has definitions
-      set_property(TARGET libbluray::libbluray APPEND PROPERTY
-                                                      INTERFACE_COMPILE_DEFINITIONS HAVE_LIBBLURAY)
-    elseif(TARGET PkgConfig::libbluray)
-      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::libbluray)
-      set_property(TARGET PkgConfig::libbluray APPEND PROPERTY
-                                                      INTERFACE_COMPILE_DEFINITIONS HAVE_LIBBLURAY)
+    elseif(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
     endif()
 
     # This is incorrectly applied to all platforms. Requires its own handling in the future
     if(NOT CORE_PLATFORM_NAME_LC STREQUAL windowsstore)
-      get_property(aliased_target TARGET "${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME}" PROPERTY ALIASED_TARGET)
-      set_property(TARGET ${aliased_target} APPEND PROPERTY
-                                                   INTERFACE_COMPILE_DEFINITIONS "HAVE_LIBBLURAY_BDJ")
+      list(APPEND ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_COMPILE_DEFINITIONS HAVE_LIBBLURAY_BDJ)
     endif()
+
+    ADD_TARGET_COMPILE_DEFINITION()
   endif()
 endif()
