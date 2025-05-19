2724b03ff8cf321d6963f412330f5ecc8f8f15a6
fuzzard <fuzzard@kodi.tv>
2025-05-19 12:51:09 +1000

    [cmake] FindPython use found TARGET rather than handling our own

--- cmake/modules/FindPython.cmake
+++ cmake/modules/FindPython.cmake
@@ -21,6 +21,13 @@
 #   ${APP_NAME_LC}::Python - The Python library
 
 if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
+
+  include(cmake/scripts/common/ModuleHelpers.cmake)
+
+  set(${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC python3)
+  set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}_DISABLE_VERSION ON)
+  SETUP_BUILD_VARS()
+
   # for Depends/Windows builds, set search root dir to libdir path
   if(KODI_DEPENDSBUILD
      OR CMAKE_SYSTEM_NAME STREQUAL WINDOWS
@@ -49,38 +56,22 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(Python3 ${VERSION} ${EXACT_VER} COMPONENTS Development ${SEARCH_QUIET})
 
   if(Python3_FOUND)
-    if(KODI_DEPENDSBUILD)
-      find_library(EXPAT_LIBRARY expat REQUIRED)
-      find_library(FFI_LIBRARY ffi REQUIRED)
-      find_library(GMP_LIBRARY gmp REQUIRED)
-      find_library(INTL_LIBRARY intl REQUIRED)
-      find_library(LZMA_LIBRARY lzma REQUIRED)
-
-      if(NOT CORE_SYSTEM_NAME STREQUAL android)
-        set(PYTHON_DEP_LIBRARIES pthread dl util)
-        if(CORE_SYSTEM_NAME STREQUAL linux)
-          # python archive built via depends requires librt for _posixshmem library
-          list(APPEND PYTHON_DEP_LIBRARIES rt)
-        endif()
-      endif()
-
-      set(Py_LINK_LIBRARIES ${EXPAT_LIBRARY} ${FFI_LIBRARY} ${GMP_LIBRARY} ${INTL_LIBRARY} ${LZMA_LIBRARY} ${PYTHON_DEP_LIBRARIES})
-    endif()
-
     # We use this all over the place. Maybe it would be nice to keep it as a TARGET property
     # but for now a cached variable will do
     set(PYTHON_VERSION "${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}" CACHE INTERNAL "" FORCE)
 
-    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
-    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
-                                                                     IMPORTED_LOCATION "${Python3_LIBRARIES}"
-                                                                     INTERFACE_INCLUDE_DIRECTORIES "${Python3_INCLUDE_DIRS}"
-                                                                     INTERFACE_LINK_OPTIONS "${Python3_LINK_OPTIONS}"
-                                                                     INTERFACE_COMPILE_DEFINITIONS HAS_PYTHON)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS Python3::Python)
 
-    if(Py_LINK_LIBRARIES)
-      set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
-                                                                       INTERFACE_LINK_LIBRARIES "${Py_LINK_LIBRARIES}")
+    if(KODI_DEPENDSBUILD)
+      # FFI isnt added to python pkgconfig, so manually add to target link libraries
+      # Presumably this is due to our use of MODULE_BUILDTYPE being static and the _ctypes module
+      find_library(FFI_LIBRARY ffi REQUIRED)
+      set_property(TARGET Python3::Python APPEND PROPERTY
+                                                 INTERFACE_LINK_LIBRARIES "${FFI_LIBRARY}")
     endif()
+
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_COMPILE_DEFINITIONS HAS_PYTHON)
+
+    ADD_TARGET_COMPILE_DEFINITION()
   endif()
 endif()
